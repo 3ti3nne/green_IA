ae9e89480588701f516981fbae358be2c1cc81a6
Etienne <eti3nne.roubaud@gmail.com>
2023-06-19 14:22:29 +0200

    second final commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# green_IA
+# Green IA
+
+Tweaking a simple ML Model in Python to try and minimize time and resources while increasing performance and accuracy
+
+## first tries
+
+1. changed dtype in the model from float32 to 16, reduced precision but increased accuracy, memory gains and calculation speed
+
+2. tried tweaking normalization of pixels by changing dividing number, changed accuracy but not a good way apparently
+
+3. teaks in the Dense class, by tweaking the neurons number we can play with the capacity of the model to learn complex things more efficiently, but it can also cause an "overlearning" and reduce the accuracy
+
+4. changed the optimizer, gained a lot of accuracy by changing adam to RMSprop, the optimizer is the algorithm that launches when we compile and train the model
--- exoGreenIa.py
+++ exoGreenIa.py
@@ -1,26 +1,50 @@
 import tensorflow as tf
-from tensorflow.keras.datasets import cifar10
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+
 # Set mixed precision policy
-tf.keras.mixed_precision.set_global_policy('mixed_float16')
+# => utilise des opérations de calcul en demi-precision (float16)
+# accelere l'entrainement tout en maintenant une precision
+tf.keras.mixed_precision.set_global_policy("mixed_float16")
+
 # Load data
+# charge les données ensemble train et ensemble test, x = image y = etiquettes
+# cifar10 ensemble de données
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
 # Normalize pixel values
-x_train, x_test = x_train / 255.0, x_test / 255.0
+# normalise les valeurs des pixels en les divisant par
+x_train, x_test = x_train / 50.0, x_test / 50.0
+
 # Define model
-model = Sequential([
-  Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
-  MaxPooling2D((2, 2)),
-  Conv2D(64, (3, 3), activation='relu'),
-  MaxPooling2D((2, 2)),
-  Conv2D(64, (3, 3), activation='relu'),
-  Flatten(),
-  Dense(64, activation='relu'),
-  Dense(10, dtype='float32')
-])
+# check différentes parties de l'image a la loupe
+# conv => extract les caracteristiques des images en entrée
+# maxPooling => recule un peu et examine l'essentiel de plus loin
+# dense => transforme les caracteristiques extraites en représentation adaptée a la tache du modele
+
+model = Sequential(
+    [
+        Conv2D(32, (3, 3), activation="ELU", input_shape=(32, 32, 3)),
+        MaxPooling2D((2, 2)),
+        Conv2D(64, (3, 3), activation="ELU"),
+        MaxPooling2D((2, 2)),
+        Conv2D(64, (3, 3), activation="ELU"),
+        Flatten(),
+        Dense(300, activation="ELU"),
+        Dense(
+            100, dtype="float16"
+        ),  # => reduit la précision des valeurs, économise mémoire et accélère calculs
+    ]
+)
+
 # Compile and train model
-model.compile(optimizer='adam',
-       loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-       metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=10)
+# compile en specifiant l'optimiseur
+model.compile(
+    optimizer="RMSprop",
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
+
+# train le model avec 10 epochs en utilisant l'algo d'opti
+model.fit(x_train, y_train, validation_split=0.2, epochs=10)
